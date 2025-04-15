e0e1c0fa5cd4520ed3357b6300dbc3fe114da427
Raghu Betina <raghu@firstdraft.com>
2025-04-15 14:55:38 -0700

    Set root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "chats#index"
   resources :messages
   resources :chats
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
